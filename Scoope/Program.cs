7251e2125c31f28dfee1b3d9eb93e6ed513671d5
Shadab Khan <shadabkhan7253@gmail.com>
2024-06-04 11:35:09 +0530

    scoope done

---
+++ Scoope/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static int a = 5;
+        static int AccessibleInt = 7;
+        static void Main(string[] args)
+        {
+            int b = 15;
+            int accessibleInt = 5;
+            Console.WriteLine(accessibleInt);
+            Console.WriteLine(AccessibleInt);
+            Console.WriteLine(Add(a,b));
+        }
+
+        static private int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
