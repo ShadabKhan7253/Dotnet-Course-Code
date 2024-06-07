28876e01d2eea3f726918ab68daa82271cee659e
Shadab Khan <shadabkhan7253@gmail.com>
2024-06-07 19:21:09 +0530

    fileOperation done

---
+++ FileOperation/Program.cs
@@ -0,0 +1,25 @@
+﻿
+namespace FileOperation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "I am writing content to he file.";
+
+            File.WriteAllText("log.txt", "\n" + str + "\n");
+
+            using StreamWriter writeFile = new("log.txt", append: true);
+
+            writeFile.WriteLine("\n" + str + "\n");
+
+            writeFile.Close();
+
+            string readFile = File.ReadAllText("log.txt");
+
+            Console.WriteLine(readFile);
+
+        }
+
+    }
+}
