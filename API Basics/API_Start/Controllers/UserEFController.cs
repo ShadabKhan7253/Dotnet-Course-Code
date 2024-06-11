13ee6ade980ac20628e0bba2e8ca6320971b7e4e
Shadab Khan <shadabkhan7253@gmail.com>
2024-06-11 22:25:19 +0530

    Controller added with entity framework setup

---
+++ API Basics/API_Start/Controllers/UserEFController.cs
@@ -0,0 +1,105 @@
+using API_Start.Data;
+using API_Start.Dtos;
+using API_Start.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Start.Controllers;
+
+[ApiController] // it will help to send the json data and receive json data  
+[Route("[controller]")]
+public class UserEFController : ControllerBase 
+{
+    DataContextEF _entityFramework;
+    public UserEFController(IConfiguration config) 
+    {
+        _entityFramework = new DataContextEF(config);
+    }
+
+    [HttpGet("GetUsers")]
+
+    public IEnumerable<User> GetUsers()
+    {
+        IEnumerable<User> users = _entityFramework.Users.ToList<User>();
+        return users;
+    }
+
+    [HttpGet("GetSingleUser/{userId}")] 
+    public User GetSingleUsers(int userId)
+    {
+        // Here FirstOrDefault() return a single user if exist otherwise it return a null value therefore we have to make User as nullable
+        User? user = _entityFramework.Users
+            .Where(u => u.UserId == userId)
+            .FirstOrDefault();
+
+        if(user != null)
+        {
+            return user; 
+        }
+        throw new Exception("Failed to get user");
+    }
+
+    [HttpPut("EditUser")]
+
+    // IActionResult return a response that tell you what happen by return chank of data here we return that request is successful or failed
+    public IActionResult EditUser(User user)
+    {
+        User? userDb = _entityFramework.Users
+            .Where(u => u.UserId == user.UserId)
+            .FirstOrDefault();
+
+        if(userDb != null)
+        {
+            userDb.FirstName = user.FirstName;
+            userDb.LastName = user.LastName;
+            userDb.Email = user.Email;
+            userDb.Gender = user.Gender;
+            userDb.Active = user.Active;
+            if(_entityFramework.SaveChanges() > 0)
+            {
+                return Ok();
+            }
+            throw new Exception("Failed to update user");
+        }
+        throw new Exception("Failed to get user");
+        
+    }
+
+    [HttpPost("AddUser")]
+    public IActionResult AddUser(UserToAddDtos user)
+    {
+        User? userDb = new User();
+
+        userDb.FirstName = user.FirstName;
+        userDb.LastName = user.LastName;
+        userDb.Email = user.Email;
+        userDb.Gender = user.Gender;
+        userDb.Active = user.Active;
+        _entityFramework.Add(userDb);
+        if(_entityFramework.SaveChanges() > 0)
+        {
+            return Ok();
+        }
+        throw new Exception("Failed to add user");
+        
+    }
+
+    [HttpDelete("DeleteUser/{userId}")]
+    public IActionResult DeleteUser(int userId)
+    {
+        User? userDb = _entityFramework.Users
+            .Where(u => u.UserId == userId)
+            .FirstOrDefault();
+
+        if(userDb != null)
+        {
+            _entityFramework.Users.Remove(userDb);
+            if(_entityFramework.SaveChanges() > 0)
+            {
+                return Ok();
+            }
+            throw new Exception("Failed to delete user");
+        }
+        throw new Exception("Failed to get user");
+
+    }
+}
--- API Basics/API_Start/obj/Debug/net8.0/API_Start.AssemblyInfo.cs
+++ API Basics/API_Start/obj/Debug/net8.0/API_Start.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_Start")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36f55c933891b3672166b6ce8c8279771ba89a33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+745810726776ed24c217b245042e4e06cab791c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_Start")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_Start")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
