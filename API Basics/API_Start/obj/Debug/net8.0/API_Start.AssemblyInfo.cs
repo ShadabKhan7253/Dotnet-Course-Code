36f55c933891b3672166b6ce8c8279771ba89a33
Shadab Khan <shadabkhan7253@gmail.com>
2024-06-11 18:40:42 +0530

    Entity Framework package added

--- API Basics/API_Start/obj/Debug/net8.0/API_Start.AssemblyInfo.cs
+++ API Basics/API_Start/obj/Debug/net8.0/API_Start.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_Start")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d973e6d690e2b6bcdceaef84ca0f53cbb3bddf2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2eaa2aeac31de2e0c803d6f6afff5dababfe67cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_Start")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_Start")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
