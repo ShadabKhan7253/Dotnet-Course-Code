2eaa2aeac31de2e0c803d6f6afff5dababfe67cf
Shadab Khan <shadabkhan7253@gmail.com>
2024-06-11 17:31:53 +0530

    DELETE method added to delete the user

--- API Basics/API_Start/obj/Debug/net8.0/API_Start.AssemblyInfo.cs
+++ API Basics/API_Start/obj/Debug/net8.0/API_Start.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_Start")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96eeb2171fbbcfa98dbb7295ff036a7c78c5c3f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d973e6d690e2b6bcdceaef84ca0f53cbb3bddf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_Start")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_Start")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
